3fec4f7c67de3a37487e3f7addd908dfa9df4763
giraudsa <giraudsa@gmail.com>
2018-06-18 16:03:53 +0200

    bug multivaluemap

--- src/main/java/com/chronosave/index/utils/MultivalueMap.java
+++ src/main/java/com/chronosave/index/utils/MultivalueMap.java
@@ -11,7 +11,7 @@ public class MultivalueMap<K, V> extends LinkedHashMap<K, LinkedHashSet<V>> {
 	private static final long serialVersionUID = -8578758310281413814L;
 
 	public V add(final K key, final V value) {
-		if (containsKey(key))
+		if (!containsKey(key))
 			put(key, new LinkedHashSet<V>());
 		get(key).add(value);
 		return value;
